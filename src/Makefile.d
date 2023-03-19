be23bbe9b74c5ccd70fd9d156cf5ccfff8eda000
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-19 15:20:09 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -428,6 +428,7 @@ $(LSP_PLUGINS_IMPULSE_REVERB_BIN)/main/ui/impulse_reverb.o: \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPlayListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ControlPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PathPort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ProxyPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Factory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIOverrides.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIContext.h \
