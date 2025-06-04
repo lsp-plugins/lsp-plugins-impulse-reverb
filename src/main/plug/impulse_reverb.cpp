5a9144f8d8dfb36b0501f99d9ae8aaec177f85a1
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-06-05 00:22:10 +0300

    Removed extra resampling

--- src/main/plug/impulse_reverb.cpp
+++ src/main/plug/impulse_reverb.cpp
@@ -1105,14 +1105,6 @@ namespace lsp
                 return status;
             }
 
-            // Try to resample
-            status  = af->resample(fSampleRate);
-            if (status != STATUS_OK)
-            {
-                lsp_trace("resample failed: status=%d (%s)", status, get_status(status));
-                return status;
-            }
-
             // Determine the normalizing factor
             size_t channels         = af->channels();
             float max = 0.0f;
@@ -1148,8 +1140,8 @@ namespace lsp
 
                 // Copy data of original sample to temporary sample and perform resampling if needed
                 dspu::Sample temp;
-                const ssize_t sample_rate_dst  = fSampleRate * dspu::semitones_to_frequency_shift(-f->fPitch);
-                if (sample_rate_dst != fSampleRate)
+                const size_t sample_rate_dst  = fSampleRate * dspu::semitones_to_frequency_shift(-f->fPitch);
+                if (sample_rate_dst != af->sample_rate())
                 {
                     if (temp.copy(af) != STATUS_OK)
                     {
